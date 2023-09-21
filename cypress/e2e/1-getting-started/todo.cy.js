a7b439021ad1053e18c199749775b106238b1975
JeremySoftware <jeremy.koedding@gmail.com>
2023-09-21 12:34:55 +0200

    I think

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -17,7 +17,7 @@ describe('textinput', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost/boulderRatten/seleniumandcypresstest/boulderRatten/index.php')
+    cy.visit('http://http://localhost/boulderRatten/seleniumandcypresstest/index.php')
   })
 
   it('can input numbers', () => {
